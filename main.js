d23efb395904a5cc019bd6bdf43b94cd610506d5
MINT11\kurio <kurionetto2004@gmail.com>
2025-01-12 02:42:36 +0900

    release?

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
-function ShowText(){
-  alert("機能しないよ");
+function ShowText(text){
+  alert(text);
+}
+
+function URLOpen(url){
+  alert('ゲームあるけどやってく？');
+  window.open(url, '_blank')
+}
+
+function FixText(){
+  document.getElementById("rainbow").innerText = "二十歳おめでとう！";
+  document.getElementById("sub_text").innerText = "え？早生まれはまだ二十歳じゃないって？\n"
+                                                  + "困った...。";
+  document.getElementById("fix_btn").innerText = "直せない...";
 }
